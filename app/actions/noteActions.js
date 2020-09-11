e33b9352e260b1b1d9943bbfb1f60f6d8df485a0
Tracy Ngo <tngo1206@gmail.com>
2020-09-11 15:41:19 -0700

    fetch action with try catch block

--- app/actions/noteActions.js
+++ app/actions/noteActions.js
@@ -3,5 +3,15 @@ import * as types from '../actionTypes/notes'
 export function fetchNotes(){
   return async dispatch => {
     disatch({type:types.FETCH_NOTES})
+try{
+    let response = await fetch('http://localhost:5000/notes');
+    if (response.status !==200){
+      throw new Error('FETCH_ERROR');
+    }
+    response = await repsonse.json();
+    dispatch({type: types.FETCH_NOTES_SUCCESS, data: response});
+  }  catch (error) {
+        dispatch({type: types.FETCH_NOTES_FAILURE, error});
+      }
+    };
   }
-}
