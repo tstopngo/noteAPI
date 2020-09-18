0187bde3db27286c43c2407de3b956ec07bb7ddd
Tracy Ngo <tngo1206@gmail.com>
2020-09-18 14:50:12 -0700

    add reducer function

--- app/reducers/notes.js
+++ app/reducers/notes.js
@@ -4,4 +4,11 @@ const INITIAL_STATE = {
   error: null,
   createStatus: null,
   createError: null,
+}
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
 };
